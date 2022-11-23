ff885f929f2b87d72f44eeea051f719da0539985
BabriWala <hanzalafarhann@gmail.com>
2022-11-23 11:01:37 +0600

    Single Person Booking Added

--- index.js
+++ index.js
@@ -71,6 +71,15 @@ async function run(){
             app.delete('/bookings/:id')
         */
 
+        // Bookings For Per Person
+        app.get('/bookings', async(req, res)=>{
+            const email = req.query.email;
+            const query = {email: email};
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+
         app.post('/bookings', async(req, res)=>{
             // console.log(req)
             const booking = req.body;
